e84565cad545ed9ef677f6d2db70563a059aa0db
yousef <yousefhalifa284@gmail.com>
2026-02-18 10:11:14 +0200

    Add vercel.json and final fixes for deployment

--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -1,11 +1,10 @@
-import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
-import { provideServerRendering, withRoutes } from '@angular/ssr';
+import { ApplicationConfig, mergeApplicationConfig } from '@angular/core';
+import { provideServerRendering } from '@angular/platform-server';
 import { appConfig } from './app.config';
-import { serverRoutes } from './app.routes.server';
 
 const serverConfig: ApplicationConfig = {
   providers: [
-    provideServerRendering(withRoutes(serverRoutes))
+    provideServerRendering()
   ]
 };
 
